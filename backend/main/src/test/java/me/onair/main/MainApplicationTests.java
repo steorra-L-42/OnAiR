2323549dcfa7dfd32a8f45f4fd9019e3ea94d2a7
허동원 <heo_dongwon@naver.com>
2024-11-01 14:22:40 +0900

    [BE] Init: Springboot "main" setup

---
+++ backend/main/src/test/java/me/onair/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package me.onair.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
