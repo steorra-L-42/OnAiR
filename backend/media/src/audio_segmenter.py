3fd21321f005779382c2cbec7e91e59eddc01378
허동원 <heo_dongwon@naver.com>
2024-11-03 22:45:24 +0900

    [BE] Feat: 세그먼트 인덱스 관리 및 마스터 플레이리스트 추가

--- backend/media/src/audio_segmenter.py
+++ backend/media/src/audio_segmenter.py
@@ -1,4 +1,8 @@
 import os
+
+from ffmpeg import output
+from yt_dlp.extractor.loom import LoomIE
+
 import config as cf
 import ffmpeg
 
@@ -7,6 +11,7 @@ import ffmpeg
 #
 def create_channels_m3u8(ch_id):
   generate_segment(ch_id)
+  create_master_m3u8(ch_id)
 
 #
 # mp3 -> HLS(1)
@@ -18,26 +23,42 @@ def generate_segment(ch_id):
   if not os.path.exists(segments_path):
     os.makedirs(segments_path)
 
+  index_no = 1
   for f in filter(lambda x: x.endswith(".mp3"), os.listdir(sources_path)):
     source_path = os.path.join(sources_path, f)
-    base_name = os.path.splitext(f)[0]
-    convert_to_hls(source_path, segments_path, ch_id, base_name)
+    segment_path = os.path.join(segments_path, str(index_no))
+
+    convert_to_hls(source_path, segment_path, index_no)
+    index_no += 1
 
 
 #
 # MP3 -> HLS(2)
 #
-def convert_to_hls(input_dir, output_dir, ch_id, base_name):
-  # base_name 을 이용하여 고유한 m3u8 파일과 세그먼트 파일 이름 생성
-  m3u8_path = os.path.join(output_dir, f'{base_name}')
+def convert_to_hls(input_dir, output_dir, index_no):
+  if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
   ffmpeg.input(input_dir).output(
-      os.path.join(output_dir, 'index.m3u8'),
+      os.path.join(output_dir, f'index{index_no}.m3u8'),
       format='hls',
       hls_time=10,
       hls_list_size=0,
       hls_flags='delete_segments',
-      hls_segment_filename= f"{output_dir}/index{ch_id}_%d.ts"
+      hls_segment_filename= f"{output_dir}/segment_{index_no}_%d.ts"
   ).run(overwrite_output=True)
 
   return output_dir
+
+#
+# master.m3u8 생성
+#
+def create_master_m3u8(ch_id):
+  segments_path = os.path.join(cf.ch_dirs[ch_id], cf.MEDIA_SEGMENTS)
+  master_path = cf.m3u8_dirs[ch_id]
+
+  with open(master_path, 'w') as master_file:
+    master_file.write('#EXTM3U\n')
+    for index_no in range(1, len(os.listdir(segments_path))):
+      master_file.write(f'#EXT-X-STREAM-INF:BANDWIDTH=128000\n')
+      master_file.write(f'ch{ch_id}\\index{index_no}.m3u8\n')
