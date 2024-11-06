76187c442c5d607c506aac756489d62c0fe1b85c
김범중 <rlaqjawnd625@naver.com>
2024-11-06 10:52:11 +0900

    [BE] Feat: 엔티티 Repository 추가

---
+++ backend/main/src/main/java/me/onair/main/domain/user/repository/RefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package me.onair.main.domain.user.repository;
+
+import me.onair.main.domain.user.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+}
