a230b152c2e472f7d07082b0d45875d6c52a7c00
허동원 <heo_dongwon@naver.com>
2024-11-06 11:02:59 +0900

    [BE] Feat: HLS 세그먼트 디렉토리로부터 stream_name 추출 함수 구현

--- backend/media/src/directory_manager.py
+++ backend/media/src/directory_manager.py
@@ -12,3 +12,15 @@ def reset_stream_path(stream_path):
   if os.path.exists(stream_path):
     shutil.rmtree(stream_path)
   os.makedirs(stream_path)
+
+
+def extract_stream_name(hls_output_path):
+  # 경로를 디렉터리로 분리하여 stream_name 추출
+  path_parts = os.path.normpath(hls_output_path).split(os.sep)
+
+  # 경로에서 'streaming_channels' 뒤의 값을 stream_name으로 사용
+  if 'streaming_channels' in path_parts:
+    stream_index = path_parts.index('streaming_channels') + 1
+    if stream_index < len(path_parts):
+      return path_parts[stream_index]
+  return None
