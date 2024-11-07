7bc4df20c9335b99fb399d816285d1072970fb5a
이철민 <lstork48@gmail.com>
2024-11-07 11:58:45 +0900

    [AND] Feat: api 인터셉터 추가 - 인터넷 권한 추가 - api 통신 시 사용할 인터셉터 추가 - 회원가입 완료 후 헤더 부분 수정할 예정 - data 모듈의 NetworkModule -> ApiModule로 변경 - network 모듈에서 PublicApi와 NetworkModule 및 TokenManager 추가 - 추후 토큰 등 수정할 예정

--- android/onair/core/network/src/main/java/com/fm404/onair/core/network/interceptor/LoggingInterceptor.kt
+++ android/onair/core/network/src/main/java/com/fm404/onair/core/network/interceptor/LoggingInterceptor.kt
@@ -1,2 +1,39 @@
 package com.fm404.onair.core.network.interceptor
 
+import android.util.Log
+import com.fm404.onair.BuildConfig
+import okhttp3.*
+import javax.inject.Inject
+
+class LoggingInterceptor @Inject constructor() : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+
+        if (BuildConfig.DEBUG) {
+            Log.d("API", "-->")
+            Log.d("API", "URL: ${request.url}")
+            Log.d("API", "Method: ${request.method}")
+            request.headers.forEach { (name, value) ->
+                Log.d("API", "Header: $name: $value")
+            }
+            request.body?.let {
+                Log.d("API", "Body: ${it.toString()}")
+            }
+        }
+
+        val response = chain.proceed(request)
+
+        if (BuildConfig.DEBUG) {
+            Log.d("API", "<--")
+            Log.d("API", "Code: ${response.code}")
+            response.headers.forEach { (name, value) ->
+                Log.d("API", "Header: $name: $value")
+            }
+            response.body?.let {
+                Log.d("API", "Body: ${it.toString()}")
+            }
+        }
+
+        return response
+    }
+}
