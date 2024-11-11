2735e8ed18599e919ddfbc492fe770534b84097d
김범중 <rlaqjawnd625@naver.com>
2024-11-12 04:18:55 +0900

    [BE] Feat: Graceful Shutdown 구현 - SIGINT 및 SIGTERM 신호 처리하여 Consumer 및 Producer 종료 처리

--- backend/station/src/main.py
+++ backend/station/src/main.py
@@ -1,13 +1,36 @@
 import logging
+import signal
 import time
+from threading import Thread
 
-from src.channel_manager import listen_for_channel_creation
+from src import consumer_manager, instance
+
+
+def handle_shutdown(signum, frame):
+    """서버 종료 시 안전하게 Consumer 정리"""
+    logging.info(f"Received shutdown signal: {signum}. Shutting down...")
+    consumer_manager.close_all_consumers()
+    logging.info("All consumers closed successfully.")
+    producer.close()
+    logging.info("Producer closed successfully.")
+    exit(0)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', force=True)
-    while True:  # 계속 실행되도록 무한 루프
-        try:
-            listen_for_channel_creation()
-        except Exception as e:
-            logging.error(f"An error occurred: {e}")
+
+    # Graceful Shutdown을 위한 신호 처리기 등록
+    signal.signal(signal.SIGINT, handle_shutdown)
+    signal.signal(signal.SIGTERM, handle_shutdown)
+
+    consumer_thread = Thread(target=consumer_manager.create_consumers, daemon=True)
+    consumer_thread.start()
+
+    producer = instance.producer
+    channel_manager = instance.channel_manager
+
+    try:
+        while True:
             time.sleep(1)
+    except KeyboardInterrupt:
+        handle_shutdown(None, None)
