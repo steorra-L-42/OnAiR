732abfc67fff4e7cacae05d2dcc2073502c5d369
박동현 <jinee9523@gmail.com>
2024-11-12 14:41:44 +0900

    [BUILD] build: 오타 수정

--- backend/station/src/config.py
+++ backend/station/src/config.py
@@ -4,12 +4,10 @@ from dotenv import load_dotenv, find_dotenv
 
 # .env 파일 로드
 dotenv_path = find_dotenv()
-if dotenv_path:
-    print(dotenv_path)
-    load_dotenv(dotenv_path)
+if dotenv_path: load_dotenv(dotenv_path)
 
 # 환경 변수에서 Kafka 설정 가져오기
-bootstrap_server = os.getenv("BOOTSTRAP_SERVER")
+bootstrap_server = os.getenv("BOOTSTRAP_SERVERS")
 group_id = os.getenv("CONSUMER_GROUP_ID")
 auto_offset_reset = os.getenv("AUTO_OFFSET_RESET")
 
