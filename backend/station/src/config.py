6f8e39a3b9553726629bd5e807c5f6b7b50863f5
박동현 <jinee9523@gmail.com>
2024-11-12 14:36:38 +0900

    [BUILD] build: env주입 관련 빌드 테스트

--- backend/station/src/config.py
+++ backend/station/src/config.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv, find_dotenv
 # .env 파일 로드
 dotenv_path = find_dotenv()
 if dotenv_path:
+    print(dotenv_path)
     load_dotenv(dotenv_path)
 
 # 환경 변수에서 Kafka 설정 가져오기
