00ffa9a11dcaa8e0e62ab89cf47b578c68edd394
김범중 <rlaqjawnd625@naver.com>
2024-11-12 14:27:48 +0900

    [BE] Refactor: .env 파일이 있을 경우만 load하도록 변경

--- backend/station/src/config.py
+++ backend/station/src/config.py
@@ -1,9 +1,11 @@
 import os
 
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
 # .env 파일 로드
-load_dotenv()
+dotenv_path = find_dotenv()
+if dotenv_path:
+    load_dotenv(dotenv_path)
 
 # 환경 변수에서 Kafka 설정 가져오기
 bootstrap_server = os.getenv("BOOTSTRAP_SERVER")
