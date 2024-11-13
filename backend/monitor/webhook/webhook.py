6936ed637ff2782de88745803326dbba4a8cb49c
박동현 <jinee9523@gmail.com>
2024-11-14 02:58:31 +0900

    [BUILD] fix: 비동기 함수 수정

--- backend/monitor/webhook/webhook.py
+++ backend/monitor/webhook/webhook.py
@@ -1,35 +1,40 @@
 '''
 추가 서버의 alertmanager의 훅을 받아 작동하는 지우개입니다.
 '''
-
 import os
 from fastapi import FastAPI, Request, HTTPException
-
+import json
 
 app = FastAPI()
 
 @app.post("/alert")
-async def alert():
-    data = Request.json
-    if not data:
-        raise HTTPException(status_code=400, detail="Invalid request payload")
-    
-    if data.get("alerts"):
-        alert_name = data["alerts"][0]["labels"]["alertname"]
-        # 서브시스템의 문제인 경우, 이게 서브시스템에서 돌아가고 있기 때문에 여기서 처리할 수 있다.
-        if alert_name == "LowAvailableMemory_Sub":
-            clear_cache()
+async def alert(request: Request):
+    try:
+        body = await request.body()
+        data = json.loads(body)
+        print(data)
+        if not data:
+            raise HTTPException(status_code=400, detail="Invalid request payload")
         
-        # TODO: 메인시스템의 문제인 경우, 추가적인 요청을 보내야한다.
-        elif alert_name == "LowAvailableMemory_Main":
-            pass
+        if data.get("alerts"):
+            alert_name = data["alerts"][0]["labels"]["alertname"]
+            
+            # 서브시스템의 문제 처리
+            if alert_name == "LowAvailableMemory_Sub":
+                clear_cache()
+            
+            # TODO: 메인 시스템의 문제 처리
+            elif alert_name == "LowAvailableMemory_Main":
+                pass
 
-    return {"status": "success"}
+        return {"status": "success"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
 
 def clear_cache():
     os.system("sudo sync; echo 3 | sudo tee /proc/sys/vm/drop_caches")
 
-
-if __name__=='__main__':
+if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=5000)
