756a23cda4dacf6a1cd4068ae48545c5f4535e27
박동현 <jinee9523@gmail.com>
2024-11-14 00:53:50 +0900

    [BUILD] feat: 모니터링 웹훅 추가

---
+++ backend/monitor/webhook/webhook.py
@@ -0,0 +1,32 @@
+'''
+추가 서버의 alertmanager의 훅을 받아 작동하는 지우개입니다.
+'''
+
+import os
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+@app.post("/alert")
+async def alert():
+    data = request.json
+    if not data:
+        return '',400
+    
+    if data.get("alerts"):
+        alert_name = data["alerts"][0]["labels"]["alertname"]
+        # 서브시스템의 문제인 경우, 이게 서브시스템에서 돌아가고 있기 때문에 여기서 처리할 수 있다.
+        if alert_name == "LowAvailableMemory_Sub":
+            clear_cache()
+        
+        # TODO: 메인시스템의 문제인 경우, 추가적인 요청을 보내야한다.
+        elif alert_name == "LowAvailableMemory_Main":
+            pass
+
+def clear_cache():
+    os.system("sudo sync; echo 3 | sudo tee /proc/sys/vm/drop_caches")
+
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=5000)
