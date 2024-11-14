7c049867b8ef694f4cb9910e03edab0a07ce26cb
김범중 <rlaqjawnd625@naver.com>
2024-11-14 14:37:37 +0900

    [BE] Refactor: 채널, 회원가입 Request 모듈화

--- backend/main/src/test/java/me/onair/main/domain/story/CreateStoryTest.java
+++ backend/main/src/test/java/me/onair/main/domain/story/CreateStoryTest.java
@@ -1,10 +1,9 @@
 package me.onair.main.domain.story;
 
-import static me.onair.main.domain.channel.enums.NewsTopic.LIFE_CULTURE;
-import static me.onair.main.domain.channel.enums.Personality.SEXY;
-import static me.onair.main.domain.channel.enums.TtsEngine.TYPECAST_SENA;
 import static me.onair.main.global.error.ErrorCode.CHANNEL_NOT_FOUND;
 import static me.onair.main.global.error.ErrorCode.ENDED_CHANNEL;
+import static me.onair.main.util.RequestUtil.createNewChannelRequest;
+import static me.onair.main.util.RequestUtil.createSignupRequest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -12,7 +11,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.List;
 import me.onair.main.MainApplication;
 import me.onair.main.domain.channel.dto.CreateNewChannelRequest;
 import me.onair.main.domain.channel.entity.Channel;
@@ -67,38 +65,6 @@ public class CreateStoryTest {
     private StoryMusicRepository storyMusicRepository;
     @MockBean
     private KafkaProducer kafkaProducer;  // KafkaProducer를 모킹
-
-    // 기본적인 SignupRequest 객체를 반환하는 메소드
-    public static SignupRequest createSignupRequest() {
-        return new SignupRequest(
-                "bbamjoong",            // username
-                "hotfriedPWPW404",      // password
-                "hotfriedBbamjoong",    // nickname
-                "01099999999",          // phoneNumber
-                "123456"                // verification
-        );
-    }
-
-    // 기본적인 SignupRequest 객체를 반환하는 메소드
-    public static CreateNewChannelRequest createNewChannelRequest() {
-        // CreateNewChannelTrackRequest 객체 생성 (곡 정보 추가)
-        CreateNewChannelRequest.CreateNewChannelTrackRequest track =
-                new CreateNewChannelRequest.CreateNewChannelTrackRequest(
-                        "Sneakers",  // title
-                        "ITZY", // artist
-                        "cover_image.jpg"  // cover
-                );
-
-        // CreateNewChannelRequest 객체 생성
-        return new CreateNewChannelRequest(
-                TYPECAST_SENA,  // ttsEngine
-                SEXY,            // personality
-                LIFE_CULTURE,    // newsTopic
-                "https://www.example.com",  // thumbnail
-                "YTN",           // channelName
-                List.of(track)   // trackList에 곡 하나를 추가
-        );
-    }
 
     @Nested
     @DisplayName("[200 Ok]")
