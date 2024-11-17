7328fe2b48a8305c8ca7758990410ed838d07660
김범중 <rlaqjawnd625@naver.com>
2024-11-17 21:31:51 +0900

    [BE] Feat: 음악 다운로드 삭제 및 존재하는 음악을 플레이리스트에 추가

--- backend/station/src/music_downloader.py
+++ backend/station/src/music_downloader.py
@@ -9,9 +9,46 @@ import requests
 from playwright.sync_api import sync_playwright
 from youtubesearchpython import VideosSearch
 
-dir_prefix = ".."
-base_dir = "/medias/musics"
-base_dir_with_prefix = dir_prefix + base_dir
+from music_finder import base_dir_with_prefix, base_dir, dir_prefix
+
+
+def sanitize_string(input_str):
+    """모든 공백 및 특수 문자 제거 (한글, 영문, 숫자만 남김)"""
+    # 모든 공백 제거 후 소문자로 변환
+    sanitized = re.sub(r'\s+', '', input_str).strip().lower()
+    # 한글, 영문, 숫자를 제외한 모든 특수문자 제거
+    sanitized = re.sub(r'[^가-힣a-zA-Z0-9]', '', sanitized)
+    return sanitized
+
+
+def find_exist_file_from_keyword(title, artist, content_type='mp3'):
+    """title - artist 형태로 파일 경로를 검색하고 반환"""
+    # 입력된 title과 artist의 공백 제거
+    sanitized_title = sanitize_string(title)
+    sanitized_artist = sanitize_string(artist)
+    target_name = f"{sanitized_title}-{sanitized_artist}"
+
+    # 디렉토리 내의 파일 목록 가져오기
+    if not os.path.isdir(base_dir_with_prefix):
+        logging.info(f"경로가 존재하지 않습니다: {base_dir_with_prefix}")
+        return None
+
+    # 파일명을 공백 제거 후 검색
+    for file in os.listdir(base_dir_with_prefix):
+        if not file.endswith(f".{content_type}"):
+            continue
+
+        # 파일명에서 확장자 제거 후 공백 제거
+        file_name, ext = os.path.splitext(file)
+        # sanitized_file_name = sanitize_string(file_name)
+        sanitized_file_name = file_name.replace(" ", "").strip().lower()
+
+        # 입력된 title과 artist를 조합한 값과 비교
+        if sanitized_file_name == target_name:
+            # 일치하는 파일이 있으면 실제 파일명을 포함한 경로 반환
+            return base_dir + f"/{file}"
+
+    return None
 
 
 def download_music_with_retries(video_url):
@@ -40,7 +77,7 @@ def download_music_with_retries(video_url):
     return None
 
 
-def download_from_keyword(title, artist, cover_url):
+def download_musics(title, artist, cover_url):
     # 먼저 file_system에 있는지 확인
     file_path = find_exist_file_from_keyword(title, artist)
     # 존재하는 파일이라면
@@ -84,45 +121,6 @@ def download_from_keyword(title, artist, cover_url):
 
     except Exception as e:
         logging.error(f"An error occurred: {e}")
-
-
-def sanitize_string(input_str):
-    """모든 공백 및 특수 문자 제거 (한글, 영문, 숫자만 남김)"""
-    # 모든 공백 제거 후 소문자로 변환
-    sanitized = re.sub(r'\s+', '', input_str).strip().lower()
-    # 한글, 영문, 숫자를 제외한 모든 특수문자 제거
-    sanitized = re.sub(r'[^가-힣a-zA-Z0-9]', '', sanitized)
-    return sanitized
-
-
-def find_exist_file_from_keyword(title, artist, content_type='mp3'):
-    """title - artist 형태로 파일 경로를 검색하고 반환"""
-    # 입력된 title과 artist의 공백 제거
-    sanitized_title = sanitize_string(title)
-    sanitized_artist = sanitize_string(artist)
-    target_name = f"{sanitized_title}-{sanitized_artist}"
-
-    # 디렉토리 내의 파일 목록 가져오기
-    if not os.path.isdir(base_dir_with_prefix):
-        logging.info(f"경로가 존재하지 않습니다: {base_dir_with_prefix}")
-        return None
-
-    # 파일명을 공백 제거 후 검색
-    for file in os.listdir(base_dir_with_prefix):
-        if not file.endswith(f".{content_type}"):
-            continue
-
-        # 파일명에서 확장자 제거 후 공백 제거
-        file_name, ext = os.path.splitext(file)
-        # sanitized_file_name = sanitize_string(file_name)
-        sanitized_file_name = file_name.replace(" ", "").strip().lower()
-
-        # 입력된 title과 artist를 조합한 값과 비교
-        if sanitized_file_name == target_name:
-            # 일치하는 파일이 있으면 실제 파일명을 포함한 경로 반환
-            return base_dir + f"/{file}"
-
-    return None
 
 
 def get_video_url_from_keyword(keyword):
